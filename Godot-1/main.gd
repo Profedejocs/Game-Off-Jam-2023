eb3fde2653ad8126e29c6da52f22ae4ff6cf1d99
shreyansh2525 <83408187+shreyansh2525@users.noreply.github.com>
2023-11-22 18:08:52 +0530

    Add files via upload

--- Godot-1/main.gd
+++ Godot-1/main.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func _process(delta):
-	$Player/Camera2D/ProgressBar.value = $Player.hp
-	$Player/Camera2D/Label.text = str(max($Player.hp, 0))+" HP"
+	$Camera2D/ProgressBar.value = $Player.hp
+	$Camera2D/Label.text = str(max($Player.hp, 0))+" HP"
