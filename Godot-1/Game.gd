bfa00882dc5a0c557cea1c4a8594a68f50f3fbd7
R9_ <neamtrares@naver.com>
2023-11-25 19:45:18 +0200

    Camera movement between rooms

--- Godot-1/Game.gd
+++ Godot-1/Game.gd
@@ -9,5 +9,10 @@ func _input(event):
 		$Camera2D.zoom.y = min($Camera2D.zoom.y*2, 1)
 
 func _process(delta):
+	if $Camera2D.zoom == Vector2.ONE:
+		$Camera2D.position.x = int($Player.global_position.x/1280)*1280
+		$Camera2D.position.y = int($Player.global_position.y/720)*720
+	else:
+		$Camera2D.position = Vector2.ZERO
 	$Control/ProgressBar.value = $Player.hp
 	$Control/Label.text = str(max($Player.hp, 0))+" HP"
