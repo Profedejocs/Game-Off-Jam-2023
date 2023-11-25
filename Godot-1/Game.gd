6947ba75826acb4114abd489d84d485dfa7b9221
R9_ <neamtrares@naver.com>
2023-11-25 19:30:56 +0200

    Zoom in and out

---
+++ Godot-1/Game.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func on_zoom_out():
+	$Worlds.position = $Worlds.position/2
+	pass
+
+func _input(event):
+	if event.is_action_pressed("zoom_out"):
+		#on_zoom_out()
+		$Camera2D.zoom.x = max($Camera2D.zoom.x/2, 0.25)
+		$Camera2D.zoom.y = max($Camera2D.zoom.y/2, 0.25)
+	if event.is_action_pressed("zoom_in"):
+		$Camera2D.zoom.x = min($Camera2D.zoom.x*2, 1)
+		$Camera2D.zoom.y = min($Camera2D.zoom.y*2, 1)
+
+func _process(delta):
+	$Control/ProgressBar.value = $Player.hp
+	$Control/Label.text = str(max($Player.hp, 0))+" HP"
--- Godot-1/main.gd
+++ Godot-1/main.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
 func _process(delta):
-	$Camera2D/ProgressBar.value = $Player.hp
-	$Camera2D/Label.text = str(max($Player.hp, 0))+" HP"
+	pass
